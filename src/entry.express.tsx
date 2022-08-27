adc9d837755f016021ae97f5d2bfb6ef1412a29f
utiyama <ninjin0604@gmail.com>
2022-08-27 18:18:20 +0900

    ポート変更

--- src/entry.express.tsx
+++ src/entry.express.tsx
@@ -1,12 +1,12 @@
-import { qwikCity } from '@builder.io/qwik-city/middleware/express';
-import express from 'express';
-import { fileURLToPath } from 'url';
-import { join } from 'path';
-import render from './entry.ssr';
+import { qwikCity } from "@builder.io/qwik-city/middleware/express";
+import express from "express";
+import { fileURLToPath } from "url";
+import { join } from "path";
+import render from "./entry.ssr";
 
 // directories where the static assets are located
-const distDir = join(fileURLToPath(import.meta.url), '..', '..', 'dist');
-const buildDir = join(distDir, 'build');
+const distDir = join(fileURLToPath(import.meta.url), "..", "..", "dist");
+const buildDir = join(distDir, "build");
 
 // create the Qwik City express middleware
 const { router, notFound } = qwikCity(render);
@@ -18,14 +18,17 @@ const app = express();
 app.use(router);
 
 // static asset handlers
-app.use(`/build`, express.static(buildDir, { immutable: true, maxAge: '1y', index: false }));
+app.use(
+  `/build`,
+  express.static(buildDir, { immutable: true, maxAge: "1y", index: false }),
+);
 app.use(express.static(distDir, { index: false }));
 
 // use Qwik City's 404 handler
 app.use(notFound);
 
 // start the express server
-app.listen(3000, () => {
+app.listen(process.env["PORT"] || 3000, () => {
   /* eslint-disable */
-  console.log(`http://localhost:3000/`);
+  console.log(`http://localhost:${process.env["PORT"] || 3000}/`);
 });
